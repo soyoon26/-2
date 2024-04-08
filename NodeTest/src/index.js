e6aaf708eceef5b269d23ca06ddc2a4e796694a3
soyoon26 <barcardi26@gmail.com>
2024-04-08 12:55:24 +0900

    2024/04/08_nodeTest

---
+++ NodeTest/src/index.js
@@ -0,0 +1,9 @@
+//const moduleData = require("./math");
+const { add, sub, multi, divide } = require("./math");
+// console.log(moduleData);
+// console.log(moduleData.add(3, 4));
+// console.log(moduleData.sub(3, 4));
+console.log(add(3, 4));
+console.log(sub(3, 4));
+console.log(multi(3, 4));
+console.log(divide(3, 4));
---
+++ NodeTest/src/math.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+function multi(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+module.exports = {
+  add,
+  sub, //key값과 같다면 이름만 적어도 된다.
+  multi,
+  divide,
+};
